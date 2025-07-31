6ec4f7d7060b8bd30527b4b5eed1a01da67a2cfd
ahmed <mohannadfathy123@gmail.com>
2025-07-31 15:52:53 +0300

    Update the display of Books

--- app/ocr/utils/DisplayBooks.js
+++ app/ocr/utils/DisplayBooks.js
@@ -92,7 +92,7 @@ function DisplayBooks({
                 )}
 
                 <div className="flex-1 text-right">
-                  <div className="font-bold text-gray-800 break-words whitespace-normal">
+                  <div className="font-bold text-gray-800 break-words break-all whitespace-normal">
                     {book.title || book.name || "بدون عنوان"}
                   </div>
                 </div>
