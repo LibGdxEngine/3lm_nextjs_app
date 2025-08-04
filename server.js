dc6673c48cf49030cf87c06110113c4b42a1f4db
ahmed <mohannadfathy123@gmail.com>
2025-08-04 14:51:41 +0300

    run the frontend as https

--- server.js
+++ server.js
@@ -1,41 +1,23 @@
-// // server.js
-// const { createServer } = require('https');
-// const { parse } = require('url');
-// const next = require('next');
-// const fs = require('fs');
-
-// const dev = process.env.NODE_ENV !== 'production';
-// const app = next({ dev });
-// const handle = app.getRequestHandler();
-
-// const httpsOptions = {
-//   key: fs.readFileSync('./cert/key.pem'),
-//   cert: fs.readFileSync('./cert/cert.pem'),
-// };
-
-// app.prepare().then(() => {
-//   createServer(httpsOptions, (req, res) => {
-//     const parsedUrl = parse(req.url, true);
-//     handle(req, res, parsedUrl);
-//   }).listen(3000, '0.0.0.0', () => {
-//     console.log('> Server running at https://0.0.0.0:3000');
-//   });
-// });
-
-// server.js (for Docker, HTTP only)
-const { createServer } = require('http'); // use http, not https
+// server.js
+const { createServer } = require('https');
 const { parse } = require('url');
 const next = require('next');
+const fs = require('fs');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+const httpsOptions = {
+  key: fs.readFileSync('./cert/key.pem'),
+  cert: fs.readFileSync('./cert/cert.pem'),
+};
+
 app.prepare().then(() => {
-  createServer((req, res) => {
+  createServer(httpsOptions, (req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
   }).listen(3000, '0.0.0.0', () => {
-    console.log('> Ready on http://0.0.0.0:3000');
+    console.log('> Server running at https://0.0.0.0:3000');
   });
 });
