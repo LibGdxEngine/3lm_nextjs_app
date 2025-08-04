bb0239b9857bfa3f340a6ba4ed651d94674c2e1f
ahmed <mohannadfathy123@gmail.com>
2025-08-04 15:16:24 +0300

    edit the config files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  compress: true, // Enable gzip compression
+  swcMinify: true, // Use the Rust-based compiler for faster builds and smaller bundles
+  experimental: {
+    scrollRestoration: true, // Optional, improves UX on back/forward navigation
+  },
+  images: {
+    unoptimized: true, // Optional: disable Next.js image optimization if you're not using it
+  },
+};
 
 export default nextConfig;
